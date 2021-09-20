f72ba49547c48bd5c023672fc4d7ddb5b5440f65
Uncle Sam <bwsamuel6@gmail.com>
2021-09-20 22:30:37 +0200

    initialize tailwindcss, craco & purgeCSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container mx-auto px-4">
+      <header className="App-header">
+        <h1>Uncle<span style={{color : "#ffb400"}}>.do</span> </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
