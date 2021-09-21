5d6529b7440d8f56dff577c433ca4d50744398e3
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2021-09-21 11:28:18 +0200

    Update Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App container mx-auto px-4 bg-black dark:bg-red">
+    <div className="App text-gray-700 dark:text-gray-50 bg-gray-100 dark:bg-gray-900">
       <header className="App-header">
-        
+        <Navbar></Navbar>
       </header>
     </div>
   );
