0dc72cf33c46af1c3a12a592fb198a4e3d38da7b
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2021-09-21 08:35:11 +0200

    Add component : Navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App container mx-auto px-4">
+    <div className="App container mx-auto px-4 bg-black dark:bg-red">
       <header className="App-header">
-        <h1>Uncle<span style={{color : "#ffb400"}}>.do</span> </h1>
+        
       </header>
     </div>
   );
