3f4ab1fcede7363d28248df018a82dd942575755
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2021-09-23 23:07:28 +0200

    Update App.js, Radio.js, Add component Todo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { Todo } from './components/Todo';
 import EmptyToDo from './components/EmptyToDo';
 import Form from './components/Form';
-function App() {
-  return (
-    <div className="App text-gray-700 dark:text-gray-50 bg-gray-100 dark:bg-gray-900">
+import { Component } from 'react';
+
+
+class App extends Component {
+    render(){
+      let element = null;
+      const empytodo = <EmptyToDo></EmptyToDo>,
+      formElt = <Form ></Form>,
+      todolist = (
+        <div className="max-w-md m-auto">
+          <p className="text-3xl text-center font-bold my-8">Votre journée aujourd'hui</p>
+          <Todo color="#ffb400" title="Etudier HTML & CSS" description="Maîtriser les tableaux"></Todo>
+          <Todo color="blue" title="Etudier Javascript" description="Maîtriser les fonctions et les boucles"></Todo>
+        </div>
+      )
+      return(<div className="App text-gray-700 dark:text-gray-50 bg-gray-100 dark:bg-gray-900">
       <header className="App-header">
         <Navbar></Navbar>
       </header>
-      {/* <EmptyToDo></EmptyToDo> */}
-      <Form ></Form>
-    </div>
-  );
+      {element}
+    </div>)
+    }
 }
 
 export default App;
--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,7 +2,7 @@
 
 export default function Radio(props){
     return(
-        <div className="radio my-3 mr-3 border-2 border-solid border-gray-900" style={{background : props.color}}>
+        <div className="radio my-3 mr-3 border-2 se border-solid border-white" style={{background : props.color}}>
             <input type="radio" />
         </div>
     )
